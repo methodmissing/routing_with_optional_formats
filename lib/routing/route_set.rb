4828cedb793f7e2bfdaa6e9e0f52d685e381ad4c
Lourens Naude <lourens@methodmissing.com>
2008-07-11 16:46:57 +0100

    eliminate map.connect as well

--- lib/routing/route_set.rb
+++ lib/routing/route_set.rb
@@ -1,5 +1,10 @@
 ActionController::Routing::RouteSet::Mapper.class_eval do
         
+  def connect(path, options = {})
+    return if path.include?( 'format' ) && options[:formatted] == false
+    @set.add_route(path, options)
+  end        
+        
   def named_route(name, path, options = {}) #:nodoc:
     return if name.include?( 'formatted' ) && options[:formatted] == false
     @set.add_named_route(name, path, options)
