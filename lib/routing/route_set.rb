bae95a8a4b7a8c58ac8325bad8de84ef38d9989c
Lourens Naude <lourens@methodmissing.com>
2008-07-11 19:38:10 +0100

    remove :formatted form options after introspection

--- lib/routing/route_set.rb
+++ lib/routing/route_set.rb
@@ -2,11 +2,13 @@ ActionController::Routing::RouteSet::Mapper.class_eval do
         
   def connect(path, options = {})
     return if path.include?( 'format' ) && options[:formatted] == false
+    options.delete(:formatted)
     @set.add_route(path, options)
   end        
         
   def named_route(name, path, options = {}) #:nodoc:
     return if name.include?( 'formatted' ) && options[:formatted] == false
+    options.delete(:formatted)
     @set.add_named_route(name, path, options)
   end
         
